8daaa34c81ba4bce6ec1ab9a4f3d195ab01fa9d6
Diogo Sousa <montezdesousa@gmail.com>
2023-08-20 17:52:07 +0100

    settings

--- openbb_sdk/sdk/core/openbb_core/app/model/system_settings.py
+++ openbb_sdk/sdk/core/openbb_core/app/model/system_settings.py
@@ -110,7 +110,7 @@ class SystemSettings(Tagged):
                 raise ValueError("Invalid logging handler")
         return v
 
-    @validator("logging_commit_hash", allow_reuse=True, always=True)
-    @classmethod
-    def validate_commit_hash(cls, v):
-        return v or get_commit_hash()
+    # @validator("logging_commit_hash", allow_reuse=True, always=True)
+    # @classmethod
+    # def validate_commit_hash(cls, v):
+    #     return v or get_commit_hash()
